b87ec944ed20c091cf4c82d48af11454c1c8be2f
Umakanthc <umakanth1808@gmail.com>
2024-12-26 11:21:05 +0530

    second changes added

--- src/main/java/com/example/demo/SpringbootFirstApplication.java
+++ src/main/java/com/example/demo/SpringbootFirstApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootFirstApplication {
 
 	public static void main(String[] args) {
+		System.out.println("sdgrrgsdff");
 		SpringApplication.run(SpringbootFirstApplication.class, args);
 	}
 
